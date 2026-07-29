82997c9464ea78988a38e25fdfe8faf80640c6bf
JANARDHAN S <jana020711@gmail.com>
2026-07-29 20:24:46 +0530

    Create install_terra.sh

---
+++ install_terra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Installing Terraform on Ubuntu..."
+sudo apt update -
+sudo apt install -y wget gpg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt install -y terraform
+echo ""
+echo "Terraform Version:"
+terraform -version
+echo ""
+echo "Terraform installation completed successfully!"
